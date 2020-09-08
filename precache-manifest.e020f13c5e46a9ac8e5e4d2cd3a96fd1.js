f2d46b45a047f260027cbf3bbc3fb61e4019e651
tetrau <tetrau01@gmail.com>
2020-09-09 01:18:12 +0800

    Updates

---
+++ precache-manifest.e020f13c5e46a9ac8e5e4d2cd3a96fd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5ebebe6ea1a00e87f17e69be310f2d",
+    "url": "/fractionator/index.html"
+  },
+  {
+    "revision": "21bdde755a084771a4e2",
+    "url": "/fractionator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c62ff7303fb9d0e8d752",
+    "url": "/fractionator/static/css/main.278954f6.chunk.css"
+  },
+  {
+    "revision": "21bdde755a084771a4e2",
+    "url": "/fractionator/static/js/2.a04c7056.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fractionator/static/js/2.a04c7056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62ff7303fb9d0e8d752",
+    "url": "/fractionator/static/js/main.2b10ae78.chunk.js"
+  },
+  {
+    "revision": "2fb98eb41dd85e0eba1f",
+    "url": "/fractionator/static/js/runtime-main.309d2ac2.js"
+  }
+]);
