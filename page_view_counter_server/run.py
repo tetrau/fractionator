99035b259e1432be0f0748ac2a2270ae766db0bd
tetrau <tetrau01@gmail.com>
2020-09-11 12:25:02 +0800

    Use environment variables to replace hard-coded URL

--- page_view_counter_server/run.py
+++ page_view_counter_server/run.py
@@ -3,14 +3,23 @@ import subprocess
 import os
 import sys
 
+hostname = sys.argv[1]
 cwd = os.path.abspath(os.path.dirname(sys.argv[0]))
 nginx_conf_file = os.path.join(cwd, "nginx.conf")
+final_nginx_conf_file = os.path.join(cwd, "nginx.{}.conf".format(hostname))
 page_view_counter_file = os.path.join(cwd, "fractionator_page_view_counter")
+
+with open(nginx_conf_file, "r") as nginx_conf_fh:
+    with open(final_nginx_conf_file, "w+") as final_nginx_conf_fh:
+        final_nginx_conf_fh.write(
+            nginx_conf_fh.read()
+            .replace("%REACT_APP_PAGE_VIEW_COUNT_HOSTNAME%", hostname)
+        )
 
 subprocess.run([
     "docker", "run", "-d", "-p", "443:443",
     "-v", "/etc/letsencrypt:/etc/letsencrypt:ro",
-    "-v", "{}:/etc/nginx/nginx.conf:ro".format(nginx_conf_file),
+    "-v", "{}:/etc/nginx/nginx.conf:ro".format(final_nginx_conf_file),
     "-v", "{}:/usr/share/nginx/html/fractionator_page_view_counter:ro".format(page_view_counter_file),
     "-v", "/etc/ssl/certs/dhparam.pem:/etc/ssl/certs/dhparam.pem:ro",
     "nginx:latest"
--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-fetch("https://utils.tetrau.io/fractionator_page_view_counter");
+if (process.env.REACT_APP_PAGE_VIEW_COUNT_URL) {
+  fetch(process.env.REACT_APP_PAGE_VIEW_COUNT_URL);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
