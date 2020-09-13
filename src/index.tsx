3865d71f31135fccd9e05f3c73137e2ef17d3f7c
tetrau <tetrau01@gmail.com>
2020-09-13 15:53:39 +0800

    Enable serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,4 @@ if (process.env.REACT_APP_PAGE_VIEW_COUNT_URL) {
   fetch(process.env.REACT_APP_PAGE_VIEW_COUNT_URL);
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
