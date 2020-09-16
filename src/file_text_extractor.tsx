aa9c8f1d5df93051ffd48b728bcbc977be18bb2b
tetrau <tetrau01@gmail.com>
2020-09-16 08:19:22 +0800

    Add page number validation to PDF file input
    Also fix a bug in PDF chapter tp page range mapping.

--- src/file_text_extractor.tsx
+++ src/file_text_extractor.tsx
@@ -83,22 +83,28 @@ interface PDFInfo {
 }
 
 export class PDFFileTextExtractorController extends React.Component<ExtractorControllerProps, {
-    pdfInfo: PDFInfo | null, pageRange: [number, number] | null, badFile: boolean, chapterSelectedIdx: number | null
+    pdfInfo: PDFInfo | null,
+    pageRange: [number, number] | null,
+    pageRangeSafe: [number, number] | null,
+    badFile: boolean,
+    chapterSelectedIdx: number | null
 }>  {
     constructor(props: ExtractorControllerProps) {
         super(props);
         this.state = {
             pdfInfo: null,
             pageRange: null,
+            pageRangeSafe: null,
             chapterSelectedIdx: null,
             badFile: false
         };
+        this.updateToSafePageRange = this.updateToSafePageRange.bind(this);
     }
 
     componentDidMount() {
         this.readPDF();
     }
-    componentDidUpdate(prevProps: ExtractorControllerProps){
+    componentDidUpdate(prevProps: ExtractorControllerProps) {
         if (prevProps.file !== this.props.file) {
             this.readPDF();
         }
@@ -123,8 +129,21 @@ export class PDFFileTextExtractorController extends React.Component<ExtractorCon
     }
 
     updatePageRange(fromPage: number, toPage: number): void {
-        this.setState({ pageRange: [fromPage, toPage] });
-        this.props.setExtractor(new PDFFileTextExtractor(this.props.file, fromPage, toPage));
+        if (this.state.pdfInfo !== null) {
+            const safeFromPage = Math.max(1, Math.min(fromPage, this.state.pdfInfo.numPages))
+            const safeToPage = Math.min(this.state.pdfInfo.numPages, Math.max(fromPage, toPage))
+            this.setState({
+                pageRange: [fromPage, toPage],
+                pageRangeSafe: [safeFromPage, safeToPage]
+            });
+            this.props.setExtractor(new PDFFileTextExtractor(this.props.file, safeFromPage, safeToPage));
+        }
+    }
+
+    updateToSafePageRange() {
+        if (this.state.pageRangeSafe !== null) {
+            this.setState({ pageRange: this.state.pageRangeSafe });
+        }
     }
 
     render() {
@@ -135,38 +154,56 @@ export class PDFFileTextExtractorController extends React.Component<ExtractorCon
         } else if (this.state.pageRange !== null && this.state.pdfInfo !== null) {
             return <div className="row mt-1">
                 <div className="col-2 pl-0 pr-1">
-                    <input type="number" className="form-control" placeholder="Page From" value={this.state.pageRange[0]}
-                        onChange={event => { this.state.pageRange !== null && this.updatePageRange(parseInt(event.target.value), this.state.pageRange[1]) }} />
+                    <div className="form-group">
+                        <label className="mb-1 ml-1 small text-nowrap">
+                                Page From
+                        </label>
+                        <input type="number" className="form-control" placeholder="Page From" value={this.state.pageRange[0]}
+                            onBlur={this.updateToSafePageRange}
+                            onChange={event => { this.state.pageRange !== null && this.updatePageRange(parseInt(event.target.value), this.state.pageRange[1]) }} />
+                    </div>
                 </div>
                 <div className="col-2 pl-0 pr-1">
-                    <input type="number" className="form-control" placeholder="Page To" value={this.state.pageRange[1]}
-                        onChange={event => { this.state.pageRange !== null && this.updatePageRange(this.state.pageRange[0], parseInt(event.target.value)) }} />
+                    <div className="form-group">
+                        <label className="mb-1 ml-1 small text-nowrap">
+                                Page To
+                        </label>
+                        <input type="number" className="form-control" placeholder="Page To" value={this.state.pageRange[1]}
+                            onBlur={this.updateToSafePageRange}
+                            onChange={event => { this.state.pageRange !== null && this.updatePageRange(this.state.pageRange[0], parseInt(event.target.value)) }} />
+                    </div>
                 </div>
                 <div className="col-8 pl-0 pr-0">
-                    <select className="form-control" value={this.state.chapterSelectedIdx === null ? -1 : this.state.chapterSelectedIdx} onChange={event => {
-                        const value: number = parseInt(event.target.value);
-                        if (value === -1) {
-                            this.setState({
-                                chapterSelectedIdx: null
-                            })
-                        } else if (this.state.pdfInfo !== null) {
-                            const chapter = this.state.pdfInfo.outline[value];
-                            this.setState(
-                                {
-                                    chapterSelectedIdx: value,
-                                }
-                            );
-                            this.updatePageRange(chapter.pageIndex, chapter.pageIndex + chapter.numPages);
-                        }
-                    }}>
-                        {this.state.pdfInfo.outline.length === 0 && <option value={-1}>No Table Of Contents</option>}
-                        {this.state.pdfInfo.outline.length > 0 && <option value={-1}>Select Chapter</option>}
-                        {this.state.pdfInfo.outline.map((chapter, idx) => {
-                            return <option key={idx} value={idx}>{chapter.title}</option>
-                        })}
-                    </select>
+                    <div className="form-group">
+                        <label className="mb-1 ml-1 small text-nowrap">
+                                Chapter
+                        </label>
+                        <select className="form-control" value={this.state.chapterSelectedIdx === null ? -1 : this.state.chapterSelectedIdx} onChange={event => {
+                            const value: number = parseInt(event.target.value);
+                            if (value === -1) {
+                                this.setState({
+                                    chapterSelectedIdx: null
+                                })
+                            } else if (this.state.pdfInfo !== null) {
+                                const chapter = this.state.pdfInfo.outline[value];
+                                this.setState(
+                                    {
+                                        chapterSelectedIdx: value,
+                                    }
+                                );
+                                console.log(chapter);
+                                this.updatePageRange(chapter.pageIndex + 1, chapter.pageIndex + 1 + chapter.numPages - 1);
+                            }
+                        }}>
+                            {this.state.pdfInfo.outline.length === 0 && <option value={-1}>No Table Of Contents</option>}
+                            {this.state.pdfInfo.outline.length > 0 && <option value={-1}>Select Chapter</option>}
+                            {this.state.pdfInfo.outline.map((chapter, idx) => {
+                                return <option key={idx} value={idx}>{chapter.title}</option>
+                            })}
+                        </select>
+                    </div>
                 </div>
-            </div>
+            </div >
         }
     }
 
