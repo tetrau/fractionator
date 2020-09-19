3935af5453265d44280f6217dd686bebb04b7b8c
tetrau <tetrau01@gmail.com>
2020-09-19 14:40:58 +0800

    Move all multi-language data to multilanguage.ts for easier editing

---
+++ src/multilanguage.ts
@@ -0,0 +1,47 @@
+export const pasteTextBtn = { "en-US": "Paste Text", "zh-CN": "粘贴文本", "zh-TW": "粘貼文本" };
+export const chooseFileBtn = { "en-US": "Choose File", "zh-CN": "选择文件", "zh-TW": "選擇文件" };
+export const fractionateBtn = { "en-US": "Fractionate", "zh-CN": "分馏", "zh-TW": "分餾" };
+export const demoBtn = { "en-US": "Demo", "zh-CN": "演示", "zh-TW": "演示" };
+export const pasteTextHere = { "en-US": "Paste Text Here", "zh-CN": "在这里粘贴文本", "zh-TW": "在這裡粘貼文本" };
+export const returnBtn = { "en-US": "Return", "zh-CN": "返回", "zh-TW": "返回" };
+export const desc = {
+    "en-US": "Fractionator is an English learning tool for English language learners. Fractionator can create a word list sorted from least common words to most common words form input text so you can learn the vocabulary of a book or a movie ahead without spoilers and have a not-interrupted-by-dictionary reading or watching experiments.",
+    "zh-CN": "分馏器·Fractionator 是一个为英语学习者提供的英语学习工具。分馏器·Fractionator 可以从输入的文本中提取出一个按照词频从最不常见到最常见的单词表。这样你就可以在不被剧透的情况下提前学习所有的词汇，获得一个不被查字典打扰的阅读或观看体验。",
+    "zh-TW": "分餾器·Fractionator 是一個為英語學習者提供的英語學習工具。分餾器·Fractionator 可以從輸入的文本中提取出一個按照詞頻從最不常見到最常見的單詞表。這樣你就可以在不被劇透的情況下提前學習所有的詞彙，獲得一個不被查字典打擾的閱讀或觀看體驗。"
+};
+
+export const instruction = {
+    "en-US": "To use Fractionator, click [Paste Text] button, then paste your input text or click [Choose File] button, choose a text file (.txt), or a subtitle file (.srt). Finally, click [Fractionate] button to fractionating result. Try It now by clicking the [Demo] button to fractionate some example text. ",
+    "zh-CN": "点击【粘贴文本】按钮，然后黏贴想要分馏的文本，或者点击【选择文件】按钮，选择一个文本文档（.txt）或字幕文档（.srt）来开始使用分馏器·Fractionator。当输入好文字或选择好文件时，点击【分馏】按钮来查看分馏的结果。点击【演示】按钮用预设的样本文本来试一试分馏器·Fractionator 的效果。",
+    "zh-TW": "點擊【粘貼文本】按鈕，然後黏貼想要分餾的文本，或者點擊【選擇文件】按鈕，選擇一個文本文檔（.txt）或字幕文檔（.srt）來開始使用分餾器·Fractionator。當輸入好文字或選擇好文件時，點擊【分餾】按鈕來查看分餾的結果。點擊【演示】按鈕用預設的樣本文本來試一試分餾器·Fractionator 的效果。"
+};
+
+export const privacyStatement = {
+    "en-US": "Fractionator is free software, all text is processed locally on your browser without been uploaded to any servers. For more information, visit the project ",
+    "zh-CN": "分馏器·Fractionator 是一个自由软件，所有的文字处理都是在浏览器本地进行的，你所输入的任何的内容都不会被上传。更多信息请访问",
+    "zh-TW": "分餾器·Fractionator 是一個自由軟件，所有的文字處理都是在瀏覽器本地進行的，你所輸入的任何的內容都不會被上傳。更多信息請訪問"
+};
+
+export const homePageLink = {
+    "en-US": "homepage",
+    "zh-CN": "项目主页",
+    "zh-TW": "項目主頁"
+};
+
+export const dictionaryOff = {
+    "en-US": "Double Click Word Show Defination: OFF",
+    "zh-CN": "双击单词显示释义：关",
+    "zh-TW": "雙擊單詞顯示釋義：關"
+};
+
+export const dictionaryChineseOn = {
+    "en-US": "Double Click Word Show Chinese Defination",
+    "zh-CN": "双击单词显示中文释义",
+    "zh-TW": "雙擊單詞顯示中文釋義"
+};
+
+export const ChineseDictionaryName = {
+    "en-US": "English-Chinese",
+    "zh-CN": "英汉",
+    "zh-TW": "英漢"
+};
